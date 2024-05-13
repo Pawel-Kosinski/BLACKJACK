876b375853073cdf3de768c4bd13e47e2692aae8
Pawel-Kosinski <p4awel123@gmail.com>
2024-05-13 18:21:46 +0200

    added betting and coins

--- Game.h
+++ Game.h
@@ -8,13 +8,28 @@ private:
     Deck deck;
     Player player;
     Player dealer;
+    int coins = 0;
+    int allCoins = 100;
 
 public:
     Game() {
         deck.shuffledeck();
     }
 
+    void bet() {
+        cout << "You have " << allCoins << " coins." << endl;
+        cout << "How much do you want to bet? " << endl;
+        cin >> coins;
+        if (coins > allCoins || coins <= 0) {
+            cout << "You can't bet that" << endl;
+            bet();
+        }
+        allCoins -= coins;
+        coins *= 2;
+    }
     void startNewGame() {
+        
+        bet();
         deck.shuffledeck();
         player.clearHand();
         dealer.clearHand();
@@ -25,7 +40,7 @@ public:
         dealer.add_card(deck.drawCard());
     }
 
-    void playerTurn() {
+    int playerTurn() {
         bool continueGame = true;
         while (continueGame) {
             std::cout << "Your hand value: " << player.getHandValue() << std::endl;
@@ -38,13 +53,14 @@ public:
                 if (player.getHandValue() > 21) {
                     std::cout << "Your hand value: " << player.getHandValue() << std::endl;
                     std::cout << "Busted" << std::endl;
-                    continueGame = false;
+                    return 1;
                 }
             }
             else if (choice == 2) {
                 continueGame = false;
             }
         }
+        return 0;
     }
 
     void dealerTurn() {
@@ -57,15 +73,18 @@ public:
         std::cout << "Dealer's hand value: " << dealer.getHandValue() << std::endl;
         if (dealer.getHandValue() > 21) {
             std::cout << "Dealer is busted. You win!" << std::endl;
+            allCoins += coins;
         }
         else if (player.getHandValue() > dealer.getHandValue()) {
             std::cout << "You win!" << std::endl;
+            allCoins += coins;
         }
         else if (player.getHandValue() < dealer.getHandValue()) {
             std::cout << "You lose!" << std::endl;
         }
         else {
             std::cout << "Tie game." << std::endl;
+            allCoins += coins / 2;
         }
     }
 
@@ -74,6 +93,10 @@ public:
         std::string response;
         std::cin >> response;
         return response == "yes";
+    }
+
+    int getAllCoins() {
+        return allCoins;
     }
 };
 #pragma once
--- main.cpp
+++ main.cpp
@@ -6,11 +6,14 @@ int main() {
     Game game;
     do {
         game.startNewGame();
-        game.playerTurn();
-        game.dealerTurn();
-        game.decideOutcome();
-    } while (game.playAgain());
-
+        int check = game.playerTurn();
+        if (check != 1) {
+            game.dealerTurn();
+            game.decideOutcome();
+        }
+    } while (game.getAllCoins() != 0 && game.playAgain());
+    if (game.getAllCoins() == 0)
+        std::cout << "You have 0 coins" << std::endl;
     std::cout << "Game Over" << std::endl;
     return 0;
 }
